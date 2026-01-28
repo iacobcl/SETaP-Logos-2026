9f311964c84fab1373425289a0bd854ff267215f
Harry Faulkner <hfaulkner2006@gmail.com>
2026-01-28 12:32:41 +0000

    Added square function

--- Team5B.py
+++ Team5B.py
@@ -1,3 +1,16 @@
-import turtle
+from turtle import Turtle
 
-#i did it!
+def square(turtle, side_length):
+    turtle.pendown()
+    turtle.forward(side_length)
+    turtle.right(90)
+    turtle.forward(side_length)
+    turtle.right(90)
+    turtle.forward(side_length)
+    turtle.right(90)
+    turtle.forward(side_length)
+    turtle.penup()
+
+if __name__ == "__main__":
+    good_boy = Turtle()
+    square(good_boy, 100)
